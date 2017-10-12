f13fe281cc656760567659b66ca83095441a1a60
judoaseeta <judoaseeta@gmail.com>
2017-10-12 16:31:15 +0900

    dependencies update

--- src/setup.js
+++ src/setup.js
@@ -1,4 +1,6 @@
 const JSDOMEnvironment = require('jest-environment-jsdom-11.0.0');
+
+// configuration for JSDOM 11.00
 new JSDOMEnvironment('');
 global.window = window;
 global.document = window.document;
